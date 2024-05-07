c7047ce0fbd8a60bee731f85d2eda3e9982eeee2
Aram Jamal <aramyamal@gmail.com>
2024-05-07 15:30:16 +0200

    restrucutured folder structure

---
+++ train_and_trace3d/train/additional_transforms.py
@@ -0,0 +1,51 @@
+from train_and_trace3d.train.generator import generate_particle
+from deeplay.applications.detection.lodestar.transforms import Transform
+import torch
+import numpy as np
+
+class RandomTranslationZ(Transform):
+    def __init__(self, dz=lambda: np.random.uniform(-3, 3)):
+        indices = (2,)
+        super().__init__(self._forward, self._backward, dz=dz, indices=indices)
+
+    @staticmethod 
+    def _forward(x, dz, indices):
+
+        radius = lambda: np.random.uniform(45e-9, 55e-9)
+        polarization_angle = lambda: np.random.rand() * 2 * np.pi
+        ph= 1/16*np.pi
+
+        batch_size = x.shape[0]
+        color_channels = 1 # DeepTrack creates grayscale images
+        image_size = 64
+        
+        transformed = torch.empty((batch_size, color_channels, image_size, image_size))
+
+        for i, dz_one in enumerate(dz):
+            image = generate_particle(image_size, z = float(dz_one), 
+                                    radius=radius, 
+                                    polarization_angle=polarization_angle, 
+                                    ph=ph)
+            image = torch.tensor(image).permute(2, 0, 1)
+            transformed[i] = image
+            
+        return transformed.to(x.device)
+        
+    @staticmethod
+    def _backward(x, dz, indices):
+        sub_v = torch.zeros_like(x)
+        sub_v[:, indices[-1]] = dz
+        return x - sub_v
+
+
+class RandomScaleImage(Transform):
+    def __init__(self, scale=lambda: np.random.uniform(0.8, 1.2)):
+        super().__init__(self._forward, self._backward, scale=scale)
+
+    @staticmethod
+    def _forward(x, scale):
+        return x * scale.view(-1, 1, 1, 1).to(x.device)
+
+    @staticmethod
+    def _backward(x, scale):
+        return x
---
+++ train_and_trace3d/train/lodestar3dtrainer.py
@@ -0,0 +1,71 @@
+from additional_transforms import *
+from deeplay.applications.detection.lodestar.transforms import *
+import deeplay as dl
+import torch
+import matplotlib.pyplot as plt
+
+class LodeSTAR3DTrainer:
+
+    def __init__(self,
+                 epochs=1000,
+                 batch_size=10,
+                 num_transforms=8,
+                 learning_rate= 1e-4,
+                 device=torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+                 model=None
+                 ):
+    
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.num_transforms = num_transforms
+        self.learning_rate = learning_rate
+        self.device = device
+        self.model = model.to(self.device)
+        self.num_outputs = 3
+        self.losses = []
+
+        self.lodestar = dl.LodeSTAR(model = self.model,
+                                    num_outputs = self.num_outputs,
+                                    n_transforms = self.num_transforms,
+                                    transforms = Transforms([
+                                                            RandomTranslationZ(),
+                                                            RandomTranslation2d(),
+                                                            RandomRotation2d(),
+                                                            RandomScaleImage(),
+                                                            ]))
+        self.model = self.lodestar._get_default_model() if model == None else model
+    
+    def train(self, image_size = 64, lower_loss_bound = None):
+        print(f"Using device: {self.device}")
+        self.losses = []
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        dataloader = torch.utils.data.DataLoader(dataset = 
+                                                 torch.empty((self.batch_size, image_size, image_size)).to(self.device))
+
+        for epoch in range(1, self.epochs+1):
+
+            for sample in dataloader:
+                transforms, inverses = self.lodestar.transform_data(sample)
+                prediction = self.lodestar.forward(transforms)
+                loss_dict = self.lodestar.compute_loss(prediction, inverses)
+                loss1, loss2 = loss_dict['between_image_disagreement'], loss_dict['within_image_disagreement']
+                loss = loss1 + loss2
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                self.losses.append(loss.item())
+
+            if lower_loss_bound != None:
+                if self.losses[epoch] < lower_loss_bound and self.losses[epoch-1] < lower_loss_bound:
+                    print(f"Epoch {epoch}/{self.epochs} \tLoss: {self.losses[-1]}")
+                    print(f"Lower loss bound of {lower_loss_bound} reached. Stopping training.")
+                    break
+        
+            if epoch % 2 == 0:
+                print(f"Epoch {epoch}/{self.epochs} \tLoss: {self.losses[-1]}", end='\r')
+        
+    def plot_losses(self, yscale='linear'):
+        plt.scatter([i for i in list(range(1, len(self.losses)+1))], self.losses, s=1, color='black')
+        plt.grid()
+        plt.yscale(yscale)
+        plt.show()
